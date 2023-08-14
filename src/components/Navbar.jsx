cc0b85539495f07d1883c105c151d8acad656363
Pinkyyy <pinkavamichal10@gmail.com>
2023-08-14 20:32:40 +0200

    Fixed search bar - lowercase and uppercase

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext, useEffect } from "react";
+import { useContext, useEffect } from "react";
 import cityData from "../city.list.json";
 import { AppContext } from "../App";
 import search from "../assets/search.svg";
@@ -9,10 +9,9 @@ export const Navbar = () => {
     setQuery,
     setQueryData,
     queryData,
+    currentWeather,
     setCurrentWeather,
     isCelsius,
-    setIsCelsius,
-    forecastWeather,
     setForecastWeather,
   } = useContext(AppContext);
 
@@ -24,12 +23,11 @@ export const Navbar = () => {
   const searchLocation = (event) => {
     if (event.key === "Enter") {
       cityData
-        .filter((city) =>
-          city.name.toLowerCase().startsWith(query.toLowerCase())
-        )
+        .filter((city) => city.name.toLowerCase().includes(query.toLowerCase()))
         .map((city) => {
-          if (query === city.name) {
+          if (query.toLowerCase() == city.name.toLowerCase()) {
             setQueryData(city);
+            console.log(city.name);
             setQuery("");
           }
         });
@@ -58,7 +56,6 @@ export const Navbar = () => {
       );
       setCurrentWeather(data[0]);
       setForecastWeather(data[1]);
-      console.log(data);
     } catch {
       console.log("Multiple fetch failed");
     }
@@ -66,12 +63,16 @@ export const Navbar = () => {
 
   useEffect(() => {
     fetchData();
-  }, [queryData, isCelsius]);
+  }, [query, isCelsius]);
 
   return (
     <nav className="navigation">
-      <img src={search} alt="" />
+      <label htmlFor="search-bar">
+        <img src={search} alt="" />
+      </label>
+
       <input
+        id="search-bar"
         type="text"
         placeholder="Search for location"
         value={query}
@@ -81,56 +82,3 @@ export const Navbar = () => {
     </nav>
   );
 };
-
-/* import { useState, useContext } from "react";
-import cityData from "../test.list.json";
-import { AppContext } from "../App";
-
-export const Navbar = () => {
-  const { query, setQuery, setData, data } = useContext(AppContext);
-
-  const api = {
-    key: "44758223122f1f02ff35afd7add211c1",
-    base: "https://api.openweathermap.org/data/2.5/",
-  };
-
-  const searchLocation = (event) => {
-    if (event.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&appid=${api.key}`)
-        .then((res) => res.json())
-        .then((data) => {
-          setData(data);
-          setQuery("");
-          console.log(data);
-        });
-      cityData.map((city) => {
-        console.log(city);
-      });
-    }
-  };
-
-  return (
-    <nav className="navigation">
-      <input
-        type="text"
-        placeholder="City..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        onKeyDown={searchLocation}
-      />
-    </nav>
-  );
-};
-*/
-
-/* 
-  useEffect(() => {
-    fetch(
-      `${api.base}forecast/daily?lat=${queryData.coord?.lat}&lon=${queryData.coord?.lon}&appid=${api.key}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        setCurrentWeather(data);
-        console.log(data);
-      });
-  }, [queryData, isCelsius]);*/
